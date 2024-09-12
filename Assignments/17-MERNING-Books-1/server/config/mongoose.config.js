b184680956978f1185bdd9385b194acfaa332f14
BluesMogul <167248881+BluesMogul@users.noreply.github.com>
2024-09-12 14:27:19 -0500

    Updated .env file to connect to MongoDB

--- Assignments/17-MERNING-Books-1/server/config/mongoose.config.js
+++ Assignments/17-MERNING-Books-1/server/config/mongoose.config.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
-
+console.log(MONGODB_URI)
 async function dbConnect() {
     try {
         await connect(MONGODB_URI, {
